6fb1f7d6e03dc097c832813b7097c986d98acd33
Markus Konttinen <markus@Markuss-MacBook-Pro.local>
2021-12-03 18:34:14 +0200

    Refactoring dont repeat yourself etc

--- day03/day03.py
+++ day03/day03.py
@@ -29,7 +29,16 @@ def part_01(binary_array):
     return gamma, epsilon
 
 
-def find_oxygen_bit(bit_sequence):
+def part_02(binary_array):
+    df = pd.DataFrame(binary_array)
+
+    df_oxygen = gas_diagnostics(binary_array, df, False)
+    df_c02 = gas_diagnostics(binary_array, df, True)
+
+    return df_oxygen, df_c02
+
+
+def find_removable_bit(bit_sequence):
     count = dict(zip(*np.unique(bit_sequence, return_counts=True)))
     if count['0'] > count['1']:
         return '1'
@@ -39,74 +48,43 @@ def find_oxygen_bit(bit_sequence):
         return 'even'
 
 
-def find_c02_bit(bit_sequence):
-    count = dict(zip(*np.unique(bit_sequence, return_counts=True)))
-    if count['0'] < count['1']:
-        return '1'
-    elif count['0'] > count['1']:
+def switch_bit(bit, is_c02):
+    if bit == '1' and is_c02:
         return '0'
-    else:
-        return 'even'
+    elif bit == '0' and is_c02:
+        return '1'
 
 
-def part_02(binary_array):
-    df_oxygen = pd.DataFrame(binary_array)
-    df_c02 = pd.DataFrame(binary_array)
-
+def gas_diagnostics(binary_array, df, is_c02):
     for idx, j in enumerate(range(0, len(binary_array[0]))):
-        if len(df_oxygen) == 1:
+        if len(df) == 1:
             break
 
-        vertical_oxygen = df_oxygen.transpose().to_numpy()
+        vertical_oxygen = df.transpose().to_numpy()
 
-        oxygen_length = len(df_oxygen)
+        df_length = len(df)
         counter = 0
-        oxygen_drop_array = []
-        oxygen_to_remove = find_oxygen_bit(vertical_oxygen[idx])
+        drop_array = []
+        bit_to_remove = find_removable_bit(vertical_oxygen[idx])
 
-        for i in range(0, oxygen_length):
+        if bit_to_remove == 'even':
+            bit_to_remove = '0'
 
-            if counter >= oxygen_length:
+        bit_to_remove = switch_bit(bit_to_remove, is_c02) if is_c02 else bit_to_remove
+
+        for i in range(0, df_length):
+
+            if counter >= df_length:
                 continue
 
-            if oxygen_to_remove == 'even':
-                oxygen_to_remove = '0'
-
-            oxygen_cell = df_oxygen.iloc[counter][idx]
-            if oxygen_cell == oxygen_to_remove:
-                oxygen_drop_array.append(df_oxygen.iloc[counter].name)
+            cell = df.iloc[counter][idx]
+            if cell == bit_to_remove:
+                drop_array.append(df.iloc[counter].name)
 
             counter += 1
 
-        df_oxygen = df_oxygen.drop(oxygen_drop_array)
-
-    for idx, j in enumerate(range(0, len(binary_array[0]))):
-        if len(df_c02) == 1:
-            break
-
-        vertical_c02 = df_c02.transpose().to_numpy()
-        c02_length = len(df_c02)
-        counter = 0
-        c02_drop_array = []
-        c02_to_remove = find_c02_bit(vertical_c02[idx])
-
-        for i in range(0, c02_length):
-
-            if counter >= c02_length:
-                continue
-
-            if c02_to_remove == 'even':
-                c02_to_remove = '1'
-
-            c02_cell = df_c02.iloc[counter][idx]
-            if c02_cell == c02_to_remove:
-                c02_drop_array.append(df_c02.iloc[counter].name)
-
-            counter += 1
-
-        df_c02 = df_c02.drop(c02_drop_array)
-
-    return df_oxygen, df_c02
+        df = df.drop(drop_array)
+    return df
 
 
 g, e = part_01(array)
@@ -115,4 +93,4 @@ print(int(g, 2) * int(e, 2))  # Part 1
 oxygen, c02 = part_02(array)
 decimal_oxygen = int(oxygen.iloc[0, :].to_string(header=False, index=False).replace("\n", ""), 2)
 decimal_c02 = int(c02.iloc[0, :].to_string(header=False, index=False).replace("\n", ""), 2)
-print(decimal_oxygen*decimal_c02)
+print(decimal_oxygen * decimal_c02)
